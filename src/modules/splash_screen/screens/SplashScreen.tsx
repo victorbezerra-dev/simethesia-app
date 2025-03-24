79270c3be2fa8f05a19a9b4e291e0cb226f8dccd
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-03-23 22:23:24 -0400

    feat: feat: Add splash screen

--- src/modules/splash_screen/screens/SplashScreen.tsx
+++ src/modules/splash_screen/screens/SplashScreen.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React from 'react';
+import { View, Text, Image, SafeAreaView } from 'react-native';
 
 export default function SplashScreen() {
   return (
-    <View >
-      <Text >Simethesia</Text>
-      <Text >
-        Plataforma de simulação anestésica interativa
-      </Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1 bg-[#02101F]">
+      <View className="flex-1 items-center justify-center relative">
+
+        <Image
+          source={require('@/assets/images/light_burst.png')}
+          className="absolute left-1/2 top-1/2 translate-y-[-26%] -translate-x-1/2 z-0 w-[700px]"
+          resizeMode="contain"
+        />
+
+        <Image
+          source={require('@/assets/images/simethesia-logo.png')}
+          className="absolute left-1/2 top-1/2 translate-y-[-110%] -translate-x-1/2 z-10 w-[200px] h-[200px]"
+          resizeMode="contain"
+        />
+
+        <View className="absolute left-1/2 top-1/2 translate-y-[30%] -translate-x-1/2 z-20 items-center">
+          <Text className="text-[#C9DBFF] text-center text-[40px] font-bold">
+            Simethesia
+          </Text>
+          <Text className="text-[20px] text-[#7C869A] text-center font-[Poppins_400Regular] mt-2">
+            Aprenda controle na veia.{"\n"}Literalmente. </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
