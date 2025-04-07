72b5d2b1c7a97f19a012555be24d216b4c510feb
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-07 01:08:50 -0400

    feat: Add route to settings simulation screen and add custom back button in appbar

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -1,5 +1,7 @@
 import HomeScreen from '@/modules/home/screens/HomeScreen'
+import SimulationSettingsHostScreen from '@/modules/simulation_settings/screens/SimulationSettingsHostScreen'
 import SplashScreen from '@/modules/splash_screen/screens/SplashScreen'
+import CustomBackButton from '@/shared/components/CustomBackButton'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 
@@ -7,9 +9,19 @@ const Stack = createNativeStackNavigator()
 
 export default function RootNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Splash" component={SplashScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator initialRouteName="Splash"
+      screenOptions={
+        {
+          headerTitle: () => null,
+          headerStyle: {
+            backgroundColor: '#02101F',
+          },
+          headerLeft: () => <CustomBackButton />,
+        }}>
+      <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen name="SimulationSettings" component={SimulationSettingsHostScreen} />
+
     </Stack.Navigator>
   )
 }
