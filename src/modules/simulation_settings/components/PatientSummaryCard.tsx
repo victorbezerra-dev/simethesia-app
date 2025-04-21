54712b7272a66466832c7bce9b9b12b8c009034c
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-20 23:52:50 -0400

    feat(patient): Add PatientSummaryCard to confirm patient data and enter total time

---
+++ src/modules/simulation_settings/components/PatientSummaryCard.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
+import CustomTextInput from "@/shared/components/CustomTextInput";
+
+const PatientSummaryCard  = () => {
+    const [totalTime, setTotalTime] = useState('');
+
+    return (
+        <View style={styles.card}>
+            <Text style={styles.label}>
+                <Text style={styles.bold}>Paciente:</Text> 30 anos, 1.58m, 75kg, Masculino
+            </Text>
+            <Text style={styles.label}>
+                <Text style={styles.bold}>Desafios:</Text> 3 ativos
+            </Text>
+
+            <CustomTextInput
+                label="Tempo Total"
+                value={totalTime}
+                unit="segundos"
+                placeholder="Ex: 32 segundos"
+                onChange={setTotalTime}
+                keyboardType="numeric"
+                labelStyle={{
+                    color: '#093361'
+                }}
+                inputStyle={{
+                    color: '#7D7F81',
+                    fontWeight: '500'
+                }}
+                icon={<Icon name="time" size={20} color="#093361" />}
+            />
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    card: {
+        padding: 16,
+        borderRadius: 12,
+    },
+    label: {
+        fontSize: 14,
+        color: '#111',
+        marginBottom: 4,
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+    section: {
+        marginTop: 16,
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 6,
+    },
+    icon: {
+        marginRight: 6,
+    },
+    sectionTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    value: {
+        fontSize: 16,
+        color: '#999',
+        fontWeight: 'bold',
+        marginBottom: 4,
+    },
+    divider: {
+        height: 1,
+        backgroundColor: '#ccc',
+        width: '100%',
+    },
+});
+export default PatientSummaryCard 
