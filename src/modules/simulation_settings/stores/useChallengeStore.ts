db574408d5e151fcd63c4713afd567f274483aa6
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-21 19:07:55 -0400

    feat: Add store to manage challenges using zustand

---
+++ src/modules/simulation_settings/stores/useChallengeStore.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+import { Challenge } from '@/shared/models/Challenge';
+import { EffectOption, effectOptions } from '@/shared/models/EffectOption';
+
+type ChallengeStore = {
+    challenges: Challenge[];
+    setReordered: (list: Challenge[]) => void; 
+    addChallenge: (effect: EffectOption, durationInSeconds: number) => void;
+    removeChallenge: (id: string) => void;
+    clearChallenges: () => void;
+    
+};
+
+export const useChallengeStore = create<ChallengeStore>((set) => ({
+    challenges: [
+        Challenge.create(effectOptions[0], 30),
+        Challenge.create(effectOptions[1], 45),
+        Challenge.create(effectOptions[2], 45),
+        Challenge.create(effectOptions[3], 60),
+        Challenge.create(effectOptions[4], 60),
+    ],
+    setReordered: (list) =>
+        set(() => ({
+          challenges: list,
+        })),
+    addChallenge: (effect, durationInSeconds) =>
+        set((state) => ({
+            challenges: [
+                ...state.challenges,
+                Challenge.create(effect, durationInSeconds),
+            ],
+        })),
+    removeChallenge: (id) =>
+        set((state) => ({
+            challenges: state.challenges.filter((c) => c.id !== id),
+        })),
+    clearChallenges: () => set({ challenges: [] }),
+}));
