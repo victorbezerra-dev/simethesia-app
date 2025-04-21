98308792c721d6fcb8fd5697de2426a0bf4d124a
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-21 18:57:13 -0400

    feat: Add GestureHandlerRootView in app module to handle complex gestures

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import "../styles/global.css"
-import  RootNavigator  from './navigation'
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; 
+import "../styles/global.css";
+import RootNavigator from './navigation';
 
 export default function App() {
-  
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
-  )
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
 }
