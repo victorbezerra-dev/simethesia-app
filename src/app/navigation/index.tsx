66ba21b9e18dd13ad228d3879a3baa6e4a4e6d48
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-27 18:47:16 -0400

    feat: Add AddNewChallenge screen in StackNavigator

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -1,4 +1,5 @@
 import HomeScreen from '@/modules/home/screens/HomeScreen'
+import AddNewChallenge from '@/modules/simulation_settings/screens/AddNewChallenge'
 import SimulationSettingsHostScreen from '@/modules/simulation_settings/screens/SimulationSettingsHostScreen'
 import SplashScreen from '@/modules/splash_screen/screens/SplashScreen'
 import CustomBackButton from '@/shared/components/CustomBackButton'
@@ -21,7 +22,7 @@ export default function RootNavigator() {
       <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}} />
       <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
       <Stack.Screen name="SimulationSettings" component={SimulationSettingsHostScreen} />
-
+      <Stack.Screen name="AddNewChallenge" component={AddNewChallenge} />
     </Stack.Navigator>
   )
 }
