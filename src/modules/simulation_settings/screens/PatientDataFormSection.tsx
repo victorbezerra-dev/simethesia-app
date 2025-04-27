ecc614405057f12af0c5663516e4cc9116931ef8
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-04-27 19:13:59 -0400

    fix: Resolve scroll issue caused by nested ScrollViews

--- src/modules/simulation_settings/screens/PatientDataFormSection.tsx
+++ src/modules/simulation_settings/screens/PatientDataFormSection.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
-import CustomTextInput from '@/shared/components/CustomTextInput';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { View, Image, LogBox } from 'react-native';
 import { CustomDropdown } from "@/shared/components/CustomDropDown";
-import { Image, View } from 'react-native';
+import { CustomTextInput } from "@/shared/components/CustomTextInput";
 
 type Props = {
   onDataChange: (data: any) => void;
@@ -15,74 +15,78 @@ export default function PatientDataFormSection({ onDataChange }: Props) {
   const [height, setHeight] = useState('');
 
   useEffect(() => {
+    LogBox.ignoreLogs(["VirtualizedLists should never be nested"])
+  }, [])
+
+  useEffect(() => {
     onDataChange({ gender, age, weight, height });
   }, [gender, age, weight, height]);
 
   return (
-    <KeyboardAwareFlatList
-      data={[{}]} 
-      keyExtractor={(_, index) => String(index)} 
-      renderItem={() => (
-        <View style={{ padding: 1 }}> 
-          <CustomDropdown
-            label="Sexo"
-            value={gender}
-            onChange={setGender}
-            options={[
-              { label: 'Masculino', value: 'Masculino' },
-              { label: 'Feminino', value: 'Feminino' },
-            ]}
-            icon={<Image
-              source={require('@/assets/icons/icon-gender.png')}
-              style={{ width: 25, height: 18 }}
-            />}
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ padding: 16 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <CustomDropdown
+        label="Sexo"
+        value={gender}
+        onChange={setGender}
+        options={[
+          { label: 'Masculino', value: 'Masculino' },
+          { label: 'Feminino', value: 'Feminino' },
+        ]}
+        icon={
+          <Image
+            source={require('@/assets/icons/icon-gender.png')}
+            style={{ width: 25, height: 18 }}
           />
+        }
+      />
 
-          <CustomTextInput
-            label="Idade"
-            value={age}
-            unit="anos"
-            placeholder="Ex: 32 anos"
-            onChange={setAge}
-            keyboardType="numeric"
-            icon={<Image
-              source={require('@/assets/icons/icon_birthday-cake.png')}
-              style={{ width: 20, height: 20 }}
-            />}
+      <CustomTextInput
+        label="Idade"
+        value={age}
+        unit="anos"
+        placeholder="Ex: 32 anos"
+        onChange={setAge}
+        keyboardType="numeric"
+        icon={
+          <Image
+            source={require('@/assets/icons/icon_birthday-cake.png')}
+            style={{ width: 20, height: 20 }}
           />
+        }
+      />
 
-          <CustomTextInput
-            label="Peso"
-            value={weight}
-            unit="kg"
-            placeholder="Ex: 52 kg"
-            onChange={setWeight}
-            keyboardType="numeric"
-            icon={<Image
-              source={require('@/assets/icons/icon_balance.png')}
-              style={{ width: 20, height: 20 }}
-            />}
+      <CustomTextInput
+        label="Peso"
+        value={weight}
+        unit="kg"
+        placeholder="Ex: 52 kg"
+        onChange={setWeight}
+        keyboardType="numeric"
+        icon={
+          <Image
+            source={require('@/assets/icons/icon_balance.png')}
+            style={{ width: 20, height: 20 }}
           />
+        }
+      />
 
-          <CustomTextInput
-            label="Altura"
-            value={height}
-            unit="m"
-            placeholder="Ex: 1.52 m"
-            onChange={setHeight}
-            keyboardType="numeric"
-            icon={<Image
-              source={require('@/assets/icons/icon_straight-ruler.png')}
-              style={{ width: 20, height: 20 }}
-            />}
+      <CustomTextInput
+        label="Altura"
+        value={height}
+        unit="m"
+        placeholder="Ex: 1.52 m"
+        onChange={setHeight}
+        keyboardType="numeric"
+        icon={
+          <Image
+            source={require('@/assets/icons/icon_straight-ruler.png')}
+            style={{ width: 20, height: 20 }}
           />
-        </View>
-      )}
-      automaticallyAdjustContentInsets={false}
-      keyboardShouldPersistTaps="always"
-      scrollEventThrottle={10}
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      showsVerticalScrollIndicator={false}
-    />
+        }
+      />
+    </KeyboardAwareScrollView>
   );
 }
