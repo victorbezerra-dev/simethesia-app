3a04eb68a901ca0072499f0cf6931ffe3e4dd67f
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-05-02 02:23:54 -0400

    feat: Add swipe to delete on Challenge Card

--- src/modules/simulation_settings/components/ChallengeCard.tsx
+++ src/modules/simulation_settings/components/ChallengeCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
     View,
     Text,
@@ -6,38 +6,63 @@ import {
     TouchableOpacity,
     Image,
 } from 'react-native';
-import { ScaleDecorator } from 'react-native-draggable-flatlist';
 import { Challenge } from '@/shared/models/Challenge';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Gesture } from 'react-native-gesture-handler';
+import { ScaleDecorator } from 'react-native-draggable-flatlist';
+import SwipeToDeleteRow from '@/shared/components/SwipeToDeleteWrapper';
 
 type Props = {
     item: Challenge;
     drag: () => void;
     isActive: boolean;
+    onDelete: () => void;
 };
 
-const ChallengeCard = ({ item, drag, isActive }: Props) => {
+const ChallengeCard = ({ item, drag, isActive, onDelete }: Props) => {
+    const dragGesture = useMemo(
+        () =>
+            Gesture.LongPress()
+                .maxDistance(400)
+                .minDuration(100)
+                .onStart(() => {
+                    drag();
+                }),
+        [drag]
+    );
+
     return (
-        <ScaleDecorator activeScale={1.05}>
-            <TouchableOpacity
-                onLongPress={drag}
-                disabled={isActive}
-                style={[styles.card, isActive && styles.activeCard]}>
-                <Image source={item.effect.icon} style={styles.icon} />
-                <View style={styles.textContainer}>
-                    <Text style={styles.title}>{item.effect.label}</Text>
-                    <Text style={styles.description}>
-                        <Text style={styles.underline}>Efeito simulado:</Text> {item.effect.description}
-                    </Text>
-                    <Text style={styles.description}>
-                        <Text style={styles.underline}>Tempo:</Text> {item.durationInSeconds} segundos
-                    </Text>
-                </View>
-                <View style={styles.dragHandle}>
-                    <MaterialCommunityIcons name="drag-vertical" size={24} color="#999" />
-                </View>
-            </TouchableOpacity>
-        </ScaleDecorator>
+        <View style={{marginBottom: 12}}>
+            <ScaleDecorator activeScale={1.05}>
+                <SwipeToDeleteRow onDelete={onDelete} externalGesture={dragGesture}>
+                    <TouchableOpacity
+                        onLongPress={drag}
+                        disabled={isActive}
+                        style={[styles.card, isActive && styles.activeCard]}
+                    >
+                        <Image source={item.effect.icon} style={styles.icon} />
+                        <View style={styles.textContainer}>
+                            <Text style={styles.title}>{item.effect.label}</Text>
+                            <Text style={styles.description}>
+                                <Text style={styles.underline}>Efeito simulado:</Text>{' '}
+                                {item.effect.description}
+                            </Text>
+                            <Text style={styles.description}>
+                                <Text style={styles.underline}>Tempo:</Text>{' '}
+                                {item.durationInSeconds} segundos
+                            </Text>
+                        </View>
+                        <View style={styles.dragHandle}>
+                            <MaterialCommunityIcons
+                                name="drag-vertical"
+                                size={24}
+                                color="#999"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                </SwipeToDeleteRow>
+            </ScaleDecorator>
+        </View>
     );
 };
 
@@ -48,7 +73,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#1E293B',
         borderRadius: 20,
         padding: 16,
-        marginBottom: 12,
     },
     activeCard: {
         backgroundColor: '#334155',
@@ -80,11 +104,6 @@ const styles = StyleSheet.create({
         marginLeft: 12,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    dot: {
-        color: '#fff',
-        fontSize: 12,
-        lineHeight: 10,
     },
 });
 
