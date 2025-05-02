eb9da5e6072917e130ea9f8116e2ac2704035b6a
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-05-02 02:24:33 -0400

    feat: Add PatientData entity

---
+++ src/shared/models/PatientData.tsx
@@ -0,0 +1,8 @@
+export type Gender = 'Masculino' | 'Feminino';
+
+export type PatientData = {
+  gender: Gender;         
+  ageInYears: number;       
+  weightInKg: number;      
+  heightInMeters: number;  
+};
