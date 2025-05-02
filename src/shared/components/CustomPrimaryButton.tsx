d12b2bf9c84ded3317d328257d2e4ebd03d06d24
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-05-02 02:27:33 -0400

    feat: Add circular loading and custom background support to CustomPrimaryButton

--- src/shared/components/CustomPrimaryButton.tsx
+++ src/shared/components/CustomPrimaryButton.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, ViewStyle, DimensionValue } from 'react-native';
+import { TouchableOpacity, Text, View, ViewStyle, DimensionValue, ActivityIndicator } from 'react-native';
 
 type Props = {
   label: string;
   disabled?: boolean;
+  loading?: boolean;
   onPress: () => void;
   height?: number;
   width?: DimensionValue;
   marginHorizontal?: number;
   marginBottom?: number;
   marginTop?: number;
+  backgroundColor?: string;
   fontSize?: number;
 };
 
 export default function CustomPrimaryButton({
   label,
   disabled = false,
+  loading = false, 
   onPress,
   height = 56,
   width = '100%',
@@ -23,14 +26,19 @@ export default function CustomPrimaryButton({
   marginBottom = 20,
   marginTop = 16,
   fontSize = 16,
+  backgroundColor
 }: Props) {
+  const isDisabled = disabled || loading;
+
   const buttonStyle: ViewStyle = {
     height,
     width,
     marginHorizontal,
     marginBottom,
     marginTop,
-    backgroundColor: disabled ? '#D1D5DB' : '#3E6DFF',
+    backgroundColor: isDisabled
+      ? '#D1D5DB'
+      : backgroundColor ?? '#3E6DFF',
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
@@ -41,12 +49,15 @@ export default function CustomPrimaryButton({
     <View style={{ marginHorizontal }}>
       <TouchableOpacity
         style={buttonStyle}
-        disabled={disabled}
+        disabled={isDisabled}
         onPress={onPress}
       >
-        <Text style={{ color: '#fff', fontWeight: 'bold', fontSize, textAlign: 'center' }}>
-          {label}
-        </Text>
+         {loading ? (
+          <ActivityIndicator size="small" color="#ffffff" />
+        ) : (
+          <Text style={{ color: '#fff', fontWeight: 'bold', fontSize, textAlign: 'center' }}>
+            {label}
+          </Text>)}
       </TouchableOpacity>
     </View>
   );
